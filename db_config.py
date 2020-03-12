0a08df902cf5aaebe81141b566a2b0e2fd7cc464
Bharath <bharathkrishnan99@gmail.com>
2020-03-12 19:26:33 +0530

    users and admins tables

--- db_config.py
+++ db_config.py
@@ -4,8 +4,8 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
  
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
 app.config['MYSQL_DATABASE_DB'] = 'shop'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
--- sqlQueries.sql
+++ sqlQueries.sql
@@ -14,6 +14,25 @@ CREATE TABLE `product` (
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE users (
+	userid varchar(255) NOT NULL,
+    pass varchar(255) NOT NULL,
+    PRIMARY KEY (userid)
+);
+
+CREATE TABLE admins (
+	userid varchar(255) NOT NULL,
+    pass varchar(255) NOT NULL,
+    PRIMARY KEY (userid)
+);
+
+INSERT INTO users VALUES ('bratk9','290799');
+INSERT INTO users VALUES ('hpeonix','110399');
+INSERT INTO users VALUES ('roja','210599');
+
+INSERT INTO admins VALUES ('admin1','default');
+INSERT INTO admins VALUES ('admin2','default');
+
 INSERT INTO `product` (`id`, `name`, `code`, `image`, `price`) VALUES
 (1, 'Deeane London Woman\'s Wedges', 'DLW01', 'shoes/1.jpg', 799.00),
 (2, 'Hush Puppies Men\'s Fuel Derby Formal Shoes', 'HPM02', 'shoes/2.jpg', 2950.00),
