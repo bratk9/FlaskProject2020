c36d31db8f38532a8b9a3d162eb7c46e92c19d95
Bharath <bharathkrishnan99@gmail.com>
2020-03-12 18:02:22 +0530

    ignore

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
  
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'shop'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
